512596b21e69956d4b23e63827e120d6c2fe49f3
Ann-Maksay <annaorlova.dn@gmail.com>
2023-11-17 14:31:46 +0200

    initial commit, added file structure, html part and images.

---
+++ js/data/cinemaRooms.js
@@ -0,0 +1,22 @@
+export const cinemaRooms = [
+  {
+    id: 1,
+    name: "Room 1",
+    seats: 40,
+  },
+  {
+    id: 2,
+    name: "Room 2",
+    seats: 60,
+  },
+  {
+    id: 3,
+    name: "Room 3",
+    seats: 20,
+  },
+  {
+    id: 4,
+    name: "Room 4",
+    seats: 35,
+  },
+];
---
+++ js/data/screeningList.js
@@ -0,0 +1,20 @@
+export const screeningList = {
+  yesterday: [
+    {
+      roomID: 1,
+      time: "2023-11-16T10:00:00Z",
+    },
+  ],
+  today: [
+    {
+      roomID: 3,
+      time: "2023-11-17T10:00:00Z",
+    },
+  ],
+  tomorrow: [
+    {
+      roomID: 4,
+      time: "2023-11-18T10:00:00Z",
+    },
+  ],
+};
